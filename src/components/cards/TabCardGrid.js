3b15208ba443267747c5e817a0f55e1244ef0a1c
Ankah <ankahharrison@gmail.com>
2024-01-03 14:59:02 +0000

    added RTK

--- src/components/cards/TabCardGrid.js
+++ src/components/cards/TabCardGrid.js
@@ -9,38 +9,46 @@ import { PrimaryButton as PrimaryButtonBase } from "../misc/Buttons.js";
 import { ReactComponent as StarIcon } from "../../images/star-icon.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-5.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/svg-decorator-blob-7.svg";
-
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../../redux/cartSlice";
 const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
 const Header = tw(SectionHeading)``;
 const TabsControl = tw.div`flex flex-wrap bg-gray-200 px-2 py-2 rounded leading-none mt-12 xl:mt-0`;
 
 const TabControl = styled.div`
-  ${tw`cursor-pointer px-6 py-3 mt-2 sm:mt-0 sm:mr-2 last:mr-0 text-gray-600 font-medium rounded-sm transition duration-300 text-sm sm:text-base w-1/2 sm:w-auto text-center`}
+  ${tw`w-1/2 px-6 py-3 mt-2 text-sm font-medium text-center text-gray-600 transition duration-300 rounded-sm cursor-pointer sm:mt-0 sm:mr-2 last:mr-0 sm:text-base sm:w-auto`}
   &:hover {
-    ${tw`bg-gray-300 text-gray-700`}
+    ${tw`text-gray-700 bg-gray-300`}
   }
-  ${props => props.active && tw`bg-green-500! text-gray-100!`}
+  ${(props) => props.active && tw`bg-green-500! text-gray-100!`}
   }
 `;
 
-const TabContent = tw(motion.div)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
+const TabContent = tw(
+  motion.div
+)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
 const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:pr-10 md:pr-6 lg:pr-12`;
-const Card = tw(motion.a)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
+const Card = tw(
+  motion.a
+)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
 const CardImageContainer = styled.div`
-  ${props => css`background-image: url("products/${props.imageSrc}");`}
-  ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
+  ${(props) =>
+    css`
+      background-image: url("products/${props.imageSrc}");
+    `}
+  ${tw`relative h-56 bg-center bg-cover rounded-t xl:h-64`}
 `;
 const CardRatingContainer = tw.div`leading-none absolute inline-flex bg-gray-100 bottom-0 left-0 ml-4 mb-4 rounded-full px-5 py-2 items-end`;
 const CardRating = styled.div`
-  ${tw`mr-1 text-sm font-bold flex items-end`}
+  ${tw`flex items-end mr-1 text-sm font-bold`}
   svg {
-    ${tw`w-4 h-4 fill-green-100 text-orange-400`}
+    ${tw`w-4 h-4 text-orange-400 fill-green-100`}
   }
 `;
 
 const CardHoverOverlay = styled(motion.div)`
   background-color: rgba(255, 255, 255, 0.5);
-  ${tw`absolute inset-12 grid`}
+  ${tw`absolute grid inset-12`}
 `;
 const CardButton = tw(PrimaryButtonBase)`text-sm`;
 
@@ -52,10 +60,10 @@ const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
 const CardPrice = tw.p`mt-4 text-sm font-bold`;
 
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
-  ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-64 w-64 opacity-20 transform translate-x-2/3 -translate-y-12 text-pink-400`}
+  ${tw`absolute top-0 right-0 w-64 h-64 text-pink-400 transform -translate-y-12 pointer-events-none -z-20 opacity-20 translate-x-2/3`}
 `;
 const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
-  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-80 w-80 opacity-20 transform -translate-x-2/3 text-green-500`}
+  ${tw`absolute bottom-0 left-0 text-green-500 transform pointer-events-none -z-20 h-80 w-80 opacity-20 -translate-x-2/3`}
 `;
 
 export default ({
@@ -63,36 +71,40 @@ export default ({
   tabs = {
     "Shea butter": [
       {
-        imageSrc:"shea butter-white.jpg",
+        id: 1,
+        imageSrc: "shea butter-white.jpg",
         title: "Yellow Shea butter",
         content: "1 kg",
         price: "22",
         price_retail: "25",
         rating: "5.0",
         reviews: "87",
-        url: "#"
+        url: "cart",
       },
       {
-        imageSrc:"organic shea butter.jpg",
+        id: 2,
+        imageSrc: "organic shea butter.jpg",
         title: "Shea butter pomade",
         content: "Small size",
         price: "5",
         price_retail: "6",
         rating: "4.8",
         reviews: "32",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"shea butter.jpg",
+        id: 3,
+        imageSrc: "shea butter.jpg",
         title: "Shea butter pomade",
         content: "Big size",
         price: "15",
         price_retail: "20",
         rating: "4.9",
         reviews: "89",
-        url: "#"
+        url: "#",
       },
       {
+        id: 4,
         imageSrc: "hairfood.jpeg",
         title: "Hair Food",
         content: "Big size",
@@ -100,7 +112,7 @@ export default ({
         price_retail: "25",
         rating: "4.6",
         reviews: "12",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "hair food-white.jpg",
@@ -110,7 +122,7 @@ export default ({
         price_retail: "10",
         rating: "100",
         reviews: "19",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "malti baby shea-white.jpg",
@@ -120,7 +132,7 @@ export default ({
         price_retail: "20",
         rating: "20",
         reviews: "61",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "shea-white.jpeg",
@@ -130,7 +142,7 @@ export default ({
         price_retail: "25",
         rating: "25",
         reviews: "95",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "foot cream.jpg",
@@ -140,71 +152,71 @@ export default ({
         price_retail: "6",
         rating: "61",
         reviews: "26",
-        url: "#"
-      }
+        url: "#",
+      },
     ],
     Soap: [
       {
-        imageSrc:"black soap2-white.jpg",
+        imageSrc: "black soap2-white.jpg",
         title: "Container Black Soap",
         content: "Compressed",
         price: "22",
         price_retail: "25",
         rating: "5.0",
         reviews: "87",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"black soap-white.jpg",
+        imageSrc: "black soap-white.jpg",
         title: "Black Soap",
         content: "Moulded",
         price: "7",
         price_retail: "8",
         rating: "4.8",
         reviews: "32",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"liquid soap.jpg",
+        imageSrc: "liquid soap.jpg",
         title: "Liquid Soap",
         content: "Lotion soap",
         price: "22",
         price_retail: "25",
         rating: "4.9",
         reviews: "89",
-        url: "#"
-      }
+        url: "#",
+      },
     ],
     "Essential Oils": [
       {
-        imageSrc:"baobab oil2-white.jpg",
+        imageSrc: "baobab oil2-white.jpg",
         title: "Baobab Oil",
         content: "250 ml",
         price: "20",
         price_retail: "25",
         rating: "5.0",
         reviews: "87",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"hair oil.jpg",
+        imageSrc: "hair oil.jpg",
         title: "Hair Oil",
         content: "250 ml",
         price: "20",
         price_retail: "25",
         rating: "4.8",
         reviews: "32",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"Kapok oil.jpg",
+        imageSrc: "Kapok oil.jpg",
         title: "Kapok Oil",
         content: "250 ml",
         price: "25",
         price_retail: "30",
         rating: "4.9",
         reviews: "89",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "Moringa oil.jpg",
@@ -214,7 +226,7 @@ export default ({
         price_retail: "310",
         rating: "4.6",
         reviews: "12",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "Moringa oil-white.jpg",
@@ -224,7 +236,7 @@ export default ({
         price_retail: "80",
         rating: "100",
         reviews: "19",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "neem oil.jpg",
@@ -234,7 +246,7 @@ export default ({
         price_retail: "25",
         rating: "20",
         reviews: "61",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "shea oil2-white.jpg",
@@ -244,7 +256,7 @@ export default ({
         price_retail: "25",
         rating: "25",
         reviews: "95",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "shea oil-white.jpg",
@@ -254,7 +266,7 @@ export default ({
         price_retail: "45",
         rating: "61",
         reviews: "26",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "virgin oil.jpg",
@@ -264,39 +276,39 @@ export default ({
         price_retail: "25",
         rating: "61",
         reviews: "26",
-        url: "#"
-      }
+        url: "#",
+      },
     ],
     Others: [
       {
-        imageSrc:"dawadawa tea.jpg",
+        imageSrc: "dawadawa tea.jpg",
         title: "Dawadawa tea",
         content: "Big size",
         price: "18",
         price_retail: "20",
         rating: "5.0",
         reviews: "87",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"dawadawa powder.jpg",
+        imageSrc: "dawadawa powder.jpg",
         title: "Dawadawa spices",
         content: "Specially made",
         price: "9",
         price_retail: "10",
         rating: "4.8",
         reviews: "32",
-        url: "#"
+        url: "#",
       },
       {
-        imageSrc:"ginger powder.jpg",
+        imageSrc: "ginger powder.jpg",
         title: "Ginger Poweder",
         content: "Specially made",
         price: "10",
         price_retail: "12",
         rating: "4.9",
         reviews: "89",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "pepper powder.jpg",
@@ -306,7 +318,7 @@ export default ({
         price_retail: "310",
         rating: "4.6",
         reviews: "12",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "tumeric powder.jpg",
@@ -316,7 +328,7 @@ export default ({
         price_retail: "20",
         rating: "100",
         reviews: "19",
-        url: "#"
+        url: "#",
       },
       {
         imageSrc: "honey.jpeg",
@@ -326,16 +338,20 @@ export default ({
         price_retail: "25",
         rating: "20",
         reviews: "61",
-        url: "#"
+        url: "#",
       },
     ],
     // Others: getRandomCards()
-  }
+  },
 }) => {
-
   const tabsKeys = Object.keys(tabs);
   const [activeTab, setActiveTab] = useState(tabsKeys[0]);
+  //const items = useSelector((state) => state.allCart.items);
+  const dispatch = useDispatch();
 
+  const handleAdd = (ele) => {
+    dispatch(addToCart(ele));
+  };
   return (
     <Container id="shop">
       <ContentWithPaddingXl>
@@ -343,7 +359,11 @@ export default ({
           <Header>{heading}</Header>
           <TabsControl>
             {Object.keys(tabs).map((tabName, index) => (
-              <TabControl key={index} active={activeTab === tabName} onClick={() => setActiveTab(tabName)}>
+              <TabControl
+                key={index}
+                active={activeTab === tabName}
+                onClick={() => setActiveTab(tabName)}
+              >
                 {tabName}
               </TabControl>
             ))}
@@ -356,14 +376,14 @@ export default ({
             variants={{
               current: {
                 opacity: 1,
-                scale:1,
+                scale: 1,
                 display: "flex",
               },
               hidden: {
                 opacity: 0,
-                scale:0.8,
+                scale: 0.8,
                 display: "none",
-              }
+              },
             }}
             transition={{ duration: 0.4 }}
             initial={activeTab === tabKey ? "current" : "hidden"}
@@ -371,7 +391,13 @@ export default ({
           >
             {tabs[tabKey].map((card, index) => (
               <CardContainer key={index}>
-                <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
+                <Card
+                  className="group"
+                  href={card.url}
+                  initial="rest"
+                  whileHover="hover"
+                  animate="rest"
+                >
                   <CardImageContainer imageSrc={card.imageSrc}>
                     <CardRatingContainer>
                       <CardRating>
@@ -384,16 +410,21 @@ export default ({
                       variants={{
                         hover: {
                           opacity: 1,
-                          height: "auto"
+                          height: "auto",
                         },
                         rest: {
                           opacity: 0,
-                          height: 0
-                        }
+                          height: 0,
+                        },
                       }}
                       transition={{ duration: 0.3 }}
                     >
-                      <CardButton>Add to Cart</CardButton><br/>
+                      {/* <CardButton>Add to Cart</CardButton> */}
+                      <button onClick={() => handleAdd()} className="px-8 py-3 text-sm font-bold text-gray-100 transition duration-300 bg-green-500 rounded hocus:bg-green-700 hocus:text-gray-200 focus:outline focus:outline-none ">
+                        {/* onClick={() => handleAdd(ele)} */}
+                        Add to Cart
+                      </button>
+                      <br />
                       <CardButton>View Details</CardButton>
                     </CardHoverOverlay>
                   </CardImageContainer>
@@ -500,6 +531,6 @@ export default ({
 //     }
 //   ];
 
-  // Shuffle array
+// Shuffle array
 //   return cards.sort(() => Math.random() - 0.5);
 // };
