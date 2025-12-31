4d391caa4daef5a7feb4bb0b8680e9d83ba17726
Bilal Abubakari <abubakaribilal99@gmail.com>
2025-12-31 09:26:32 +0000

    feat: add hero images, decorator blobs, dialog component, and framer-motion dependency

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,1 +1,34 @@
+// Company Logo
 export { default as CompanyLogo } from './images/logo.svg';
+
+// Hero images
+export { default as Hero0 } from './images/hero-0.jpg';
+export { default as Hero1 } from './images/hero-1.jpg';
+export { default as Hero2 } from './images/hero-2.jpg';
+export { default as Hero3 } from './images/hero-3.jpg';
+export { default as Hero4 } from './images/hero-4.jpg';
+export { default as Hero5 } from './images/hero-5.jpg';
+export { default as Hero6 } from './images/hero-6.jpg';
+export { default as Hero7 } from './images/hero-7.jpg';
+export { default as Hero8 } from './images/hero-8.jpg';
+export { default as Hero9 } from './images/hero-9.jpg';
+export { default as Hero10 } from './images/hero-10.jpg';
+export { default as Hero11 } from './images/hero-11.jpg';
+export { default as Hero12 } from './images/hero-12.jpg';
+export { default as Hero13 } from './images/hero-13.jpg';
+export { default as Hero14 } from './images/hero-14.png';
+
+// Decorator Blob images
+export { default as SvgDecoratorBlob1 } from './images/svg-decorator-blob-1.svg';
+export { default as SvgDecoratorBlob2 } from './images/svg-decorator-blob-2.svg';
+export { default as SvgDecoratorBlob3 } from './images/svg-decorator-blob-3.svg';
+export { default as SvgDecoratorBlob4 } from './images/svg-decorator-blob-4.svg';
+export { default as SvgDecoratorBlob5 } from './images/svg-decorator-blob-5.svg';
+export { default as SvgDecoratorBlob6 } from './images/svg-decorator-blob-6.svg';
+export { default as SvgDecoratorBlob7 } from './images/svg-decorator-blob-7.svg';
+export { default as SvgDecoratorBlob8 } from './images/svg-decorator-blob-8.svg';
+export { default as SvgDecoratorBlob9 } from './images/svg-decorator-blob-9.svg';
+export { default as SvgDecoratorBlob10 } from './images/svg-decorator-blob-10.svg';
+
+// Videos
+// export { default as HeroVideo } from './videos/hero.mp4';
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Home from '@/components/home/home';
 import { JSX } from 'react';
+import Home from '@/components/home/home';
 
 export default function HomePage(): JSX.Element {
   return <Home />;
