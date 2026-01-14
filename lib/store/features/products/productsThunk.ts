fbd75842acd74a180c3f706708abbd371c6f00f4
Bilal Abubakari <abubakaribilal99@gmail.com>
2026-01-14 08:14:01 +0000

    feat: enhance error handling in getBestProducts thunk and add utility for error message extraction

--- lib/store/features/products/productsThunk.ts
+++ lib/store/features/products/productsThunk.ts
@@ -1,14 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ProductResponseDto, productsControllerGetBestProducts } from '@/app/api';
+import { productsControllerGetBestProducts } from '@/app/api';
+import { getErrorMessage } from '@/lib/utils';
 
 export const getBestProducts = createAsyncThunk(
   'products/getBestProducts',
-  async (): Promise<ProductResponseDto[]> => {
+  async (_, { rejectWithValue }) => {
     try {
-      const { data } = await productsControllerGetBestProducts();
+      const { data, error } = await productsControllerGetBestProducts();
+
+      if (!data || error) {
+        return rejectWithValue(getErrorMessage(error, 'Failed to fetch best products.'));
+      }
+
       return data?.data.data ?? [];
     } catch (error) {
-      throw new Error(String(error));
+      console.log('Error here', error);
+      throw new Error(getErrorMessage(error, 'Failed to fetch best products.'));
     }
   },
 );
